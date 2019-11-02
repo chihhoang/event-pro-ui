1684d4fca1209317cd2a093afcbd3bfe72deb1e2
Chi Hoang <chhoang102@gmail.com>
2019-11-02 12:50:26 -0700

    Change app name

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({ user }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <Link className="navbar-brand" to="/">
-        Storage Manager
+        Event Pro
       </Link>
       <button
         className="navbar-toggler"
