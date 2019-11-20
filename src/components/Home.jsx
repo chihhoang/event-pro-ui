f95137a4fc44cffb5b0de4c473d8386427c7203f
manishayacham <manisha.yacham16@gmail.com>
2019-11-19 22:53:29 -0800

    added client code for list of events and addtocart/order page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import image from "../Images/image-event.jpg";
+import Image from "react-bootstrap/Image";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      {" "}
+      <h1>Home</h1>
+      <a href="/list">
+        <Image src={image} width="500px" height="500px" thumbnail />
+      </a>
+    </div>
+  );
 };
 
 export default Home;
